a25e80eaa2b43f33ad3f5c04341e96420c08a046
kevinsunil <kevinsunil.2001@gmail.com>
2022-02-10 23:39:37 +0530

    Final commit

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const app = express();
-const mongoose = require ('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-
-mongoose.connect('mongodb://localhost/blog',{useNewUrlParser: true, useUnifiedTopology: true})
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log("MongoDB database connection established successfully");
-// })
-
-app.set('view engine', 'ejs')
-
-app.use('/articles',articleRouter)
-
-app.get('/',(req,res)=> {
-    const articles = [{
-        title: 'test',
-        createdAt: new Date(),
-        description: 'this a short description'
-    }]
-    res.render("articles/index",{articles: articles})
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true
 })
 
-app.listen(5000);
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
